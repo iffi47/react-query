9e156e9b3c6d84f75c63cc590f85974a0285e483
iffi47 <iftikharlion958@gmail.com>
2026-07-12 16:24:33 +0500

    first commit

---
+++ src/components/Events/FindEventSection.jsx
@@ -0,0 +1,34 @@
+import { useQueries, useQuery } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
+import { fetchEvents } from '../../util/http';
+
+export default function FindEventSection() {
+  const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState('');
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  };
+  const query = useQuery({
+    queryKey: ['event', {search: searchElement.current.value}],
+    queryFn: () =>  fetchEvents(searchElement.current.value),
+    staleTime: 5000
+  })
+
+  return (
+    <section className="content-section" id="all-events-section">
+      <header>
+        <h2>Find your next event!</h2>
+        <form onSubmit={handleSubmit} id="search-form">
+          <input
+            type="search"
+            placeholder="Search events"
+            ref={searchElement}
+          />
+          <button>Search</button>
+        </form>
+      </header>
+      <p>Please enter a search term and to find events.</p>
+    </section>
+  );
+}
